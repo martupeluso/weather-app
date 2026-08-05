f367a364285bf573a9bd0d3b9126170f4970f9f8
Martu Peluso <martupeluso@proton.me>
2026-08-05 19:01:51 -0300

    Fetch weather info on form submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import { processWeatherData } from "./api";
 
 console.log(await processWeatherData("buenosaires"));
+
+const form = document.querySelector("form");
+const locationInput = document.querySelector("#location");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const location = locationInput.value;
+  console.log(await processWeatherData(location));
+});
