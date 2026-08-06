8165f31b16aedcc5716bb10883985ca9b3a556ff
Martu Peluso <martupeluso@proton.me>
2026-08-06 18:05:37 -0300

    Clear weather display before rendering new data

--- src/dom.js
+++ src/dom.js
@@ -5,6 +5,8 @@ const weatherDataContainer = document.querySelector(".weather-data-container");
 async function showWeatherData(location) {
   const weatherData = await processWeatherData(location);
 
+  weatherDataContainer.textContent = "";
+
   for (let info in weatherData) {
     const paragraph = document.createElement("p");
     paragraph.textContent = `${info}: ${weatherData[info]}`;
